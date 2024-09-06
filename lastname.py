daaae9d2560573272ffb1a6ecbc5ba1e659a9dc9
professor Wu <hwu@qcc.cuny.edu>
2024-09-06 12:55:29 -0400

    added a username variable

--- lastname.py
+++ lastname.py
@@ -3,4 +3,5 @@ Student's full name
 lab 2, git and github in VS code
 Sep 6
 """
-print("Welcome to Git commands ")
+username = input("Enter a username: ")
+print(f"Welcome to Git commands {username}")
